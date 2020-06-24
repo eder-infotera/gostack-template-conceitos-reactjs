76a508fb9def4ad8e246983355b0a7809a81ed30
eder <eder@infotera.com.br>
2020-06-24 10:48:06 -0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+
   async function handleAddRepository() {
     // TODO
+
+    const response = await api.post('repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Diego'
+    });
+
+    setProjects([...projects, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+
+      await api.delete(`repositories/${id}`);
+
+      setProjects(projects.filter( p => p.id != id));
+
+    } catch (ex) {
+      console.trace(ex);
+    }
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {projects.map(p =>
+          <li key={p.id}>
+            {p.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(p.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
